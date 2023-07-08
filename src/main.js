5480c5bd481843d78113e4c405172b90878cbd0d
Alex Fermam <fermam@pop-os.localdomain>
2023-07-08 17:01:33 -0400

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import '@/assets/css/main.css'
+import { createPinia } from 'pinia'
+const pinia = createPinia()
+const app = createApp(App)
+app.use(pinia)
+app.mount('#app')
